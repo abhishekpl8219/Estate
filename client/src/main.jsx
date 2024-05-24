ab233418b6a6c7684b6c2b7bea091a6d0868c34e
abhishek <abhishekpl8219@gmail.com>
2024-05-24 08:23:55 +0530

    now added the original redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,27 +1,15 @@
-<<<<<<< HEAD
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { store, persistor } from "./redux/store.js";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { persistor, store } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { store } from "./redux/store.js";
-import { Provider } from "react-redux";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
->>>>>>> 8bdef9144638dcb3dad6671b9f4c15445d1adebd
   </Provider>
 );
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,33 +1,24 @@
-<<<<<<< HEAD
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/user.slice";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import { version } from "mongoose";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/user.slice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({ user: userReducer });
 
 const persistConfig = {
-  key: "root",
+  key: 'root',
   storage,
   version: 1,
 };
-const rootReducer = combineReducers({ user: userReducer });
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-export const store = configureStore({
-  reducer: persistedReducer,
-=======
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/user.slice";
 
 export const store = configureStore({
-  reducer: { user: userReducer },
->>>>>>> 8bdef9144638dcb3dad6671b9f4c15445d1adebd
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
-<<<<<<< HEAD
 
 export const persistor = persistStore(store);
-=======
->>>>>>> 8bdef9144638dcb3dad6671b9f4c15445d1adebd
