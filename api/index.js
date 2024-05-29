6ee94a0c306947da8e7cf4fcafa6990fe47b30e0
abhishek <abhishekpl8219@gmail.com>
2024-05-29 08:16:35 +0530

    create lisitng api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/lisitng.route.js";
 
 dotenv.config();
 mongoose
@@ -24,6 +25,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
