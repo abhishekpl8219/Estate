5a8405872572caafc983dc8e5fff4d8a784f49be
abhishek <abhishekpl8219@gmail.com>
2024-06-01 12:38:59 +0530

    ready for deployment-2

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/lisitng.route.js";
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -16,6 +17,7 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -26,6 +28,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app,
+  get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
